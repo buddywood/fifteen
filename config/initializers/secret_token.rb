370ef94ea2ad66972243d8abf4b71558c6fafee4
ebilly <ebillytalton@yahoo.com>
2012-12-01 13:30:50 -0600

    init version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fameinfifteen::Application.config.secret_token = '2106104e70ae64a3d2eba1c565968cd77d6746451499fcc15d670aaa82ca6dc8016eab291717744c281f1cbf7d47acb5b7293d0fa0721806802336e2ea33770b'
